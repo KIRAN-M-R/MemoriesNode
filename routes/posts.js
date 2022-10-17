3744b09024a06a2ae0b3d841e873286c4d533b6d
KIRAN M R <iamthekiranmr@gmail.com>
2022-10-17 16:48:32 +0530

    pagination finished

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getPostsBySearch,
   getPosts,
   createPost,
   updatePost,
@@ -9,6 +10,8 @@ import {
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
+
+router.get('/search', getPostsBySearch);
 router.get("/", getPosts);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
